037d8761a375381e6cd8b442c58c40b849b75223
Kris Sum <kris@switchsystems.co.uk>
2021-04-19 21:09:46 +0100

    initial commit

---
+++ AtemAudioMonitorSwitcher/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using BMDSwitcherAPI;
+using System.Threading;
+
+namespace AtemAudioMonitorSwitcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBMDSwitcherDiscovery discovery = new CBMDSwitcherDiscovery();
+
+			AtemSwitcher atem;
+			// Connect to switcher
+			_BMDSwitcherConnectToFailure failureReason = 0;
+			try
+			{
+				discovery.ConnectTo("192.168.250.81", out IBMDSwitcher switcher, out failureReason);
+				Console.WriteLine("Connected to ATEM switcher");
+				atem = new AtemSwitcher(switcher);
+			} catch (COMException) {
+				switch (failureReason)
+				{
+					case _BMDSwitcherConnectToFailure.bmdSwitcherConnectToFailureNoResponse:
+						Console.WriteLine("No response");
+						break;
+					case _BMDSwitcherConnectToFailure.bmdSwitcherConnectToFailureIncompatibleFirmware:
+						Console.WriteLine("Incompatible firmware");
+						break;
+					default:
+						Console.WriteLine("Unable to connect: " + failureReason.ToString());
+						break;
+				}
+				Console.Write("Press ENTER to exit...");
+				Console.ReadLine();
+				return;
+			}
+			AutoResetEvent evt = new AutoResetEvent(false);
+			atem.fetchAudioInputs();
+			Console.WriteLine("Found Fairlight audio mixer");
+			evt.WaitOne();
+			Console.Write("Press ENTER to exit...");
+            Console.ReadLine();
+        }
+    }
+
+
+
+	internal class AtemSwitcher 
+	{
+		private IBMDSwitcher switcher;
+
+		private IBMDSwitcherFairlightAudioMixer audioMixer;
+		public AtemSwitcher(IBMDSwitcher switcher) => this.switcher = switcher;
+
+		private List<IBMDSwitcherFairlightAudioInput> m_allInputs; // all inputs
+		private List<IBMDSwitcherFairlightAudioSource> m_allSources; // all sources (iterated each input)
+		public void fetchAudioInputs()
+		{
+			audioMixer = (IBMDSwitcherFairlightAudioMixer)switcher;
+			audioMixer.SetAllLevelNotificationsEnabled(1);
+
+			Guid audioIteratorIID = typeof(IBMDSwitcherFairlightAudioInputIterator).GUID;
+			IntPtr audioIteratorPtr;
+			IBMDSwitcherFairlightAudioInputIterator audioIterator = null;
+			audioMixer.CreateIterator(ref audioIteratorIID, out audioIteratorPtr);
+			if (audioIteratorPtr != null)
+			{
+				audioIterator = (IBMDSwitcherFairlightAudioInputIterator)Marshal.GetObjectForIUnknown(audioIteratorPtr);
+			}
+
+			m_allInputs = new List<IBMDSwitcherFairlightAudioInput>();
+			m_allSources = new List<IBMDSwitcherFairlightAudioSource>();
+
+			int sourceCount = 0;
+
+			while (true)
+			{
+				IBMDSwitcherFairlightAudioInput audioInput;
+				audioIterator.Next(out audioInput);
+				if (audioInput == null)
+				{
+					break;
+				}
+				m_allInputs.Add(audioInput);
+				
+
+				// added all sources 
+				Guid audioSourceIteratorIID = typeof(IBMDSwitcherFairlightAudioSourceIterator).GUID;
+				IntPtr audioSourceIteratorPtr;
+				IBMDSwitcherFairlightAudioSourceIterator audioSourceIterator = null;
+				audioInput.CreateIterator(ref audioSourceIteratorIID, out audioSourceIteratorPtr);
+				if (audioSourceIteratorPtr != null)
+				{
+					audioSourceIterator = (IBMDSwitcherFairlightAudioSourceIterator)Marshal.GetObjectForIUnknown(audioSourceIteratorPtr);
+				}
+				while (true)
+				{
+					IBMDSwitcherFairlightAudioSource audioSource;
+					audioSourceIterator.Next(out audioSource);
+					if (audioSource == null)
+					{
+						break;
+					}
+					audioSource.AddCallback(new AtemAudioSource(audioInput, audioSource, sourceCount)); // add callback to monitor audio level
+					sourceCount++;
+					m_allSources.Add(audioSource);
+				}
+			}
+
+		}
+		public List<IBMDSwitcherFairlightAudioInput> GetAudioInputs()
+		{
+			return m_allInputs;
+		}
+
+		public List<IBMDSwitcherFairlightAudioSource> GetAudioSources()
+		{
+			return m_allSources;
+		}
+    }
+
+	internal class AtemAudioSource : IBMDSwitcherFairlightAudioSourceCallback
+	{
+		IBMDSwitcherFairlightAudioInput m_audioInput;
+		IBMDSwitcherFairlightAudioSource m_audioSource;
+		int lineIndex = 0;
+		long inputId;
+		long sourceId;
+		long milliseconds = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+		public AtemAudioSource(IBMDSwitcherFairlightAudioInput audioInput, IBMDSwitcherFairlightAudioSource audioSource, int index)
+        {
+			m_audioInput = audioInput;
+			m_audioSource = audioSource;
+			lineIndex = index;
+			this.m_audioInput.GetId(out inputId);
+			this.m_audioSource.GetId(out sourceId);
+		}
+
+		public void Notify(_BMDSwitcherFairlightAudioSourceEventType eventType)
+		{
+			//throw new NotImplementedException();
+		}
+
+		public void OutputLevelNotification(uint numLevels, ref double levels, uint numPeakLevels, ref double peakLevels)
+		{
+			if (DateTimeOffset.Now.ToUnixTimeMilliseconds() - milliseconds < 100)
+            {
+				return;
+            }
+			if (numLevels > 0 && levels != double.NegativeInfinity)
+			{
+				//Console.ForegroundColor = BorderColor;
+				Console.CursorTop = lineIndex + 4;
+				Console.CursorLeft = 0;
+				//Console.Write(s);
+				//Console.ResetColor();
+
+				//
+				// draw -50 to 0 - lets use 25 characters for this, so each character is 2db.
+				//
+				int characters = 50 + (int) Math.Round(levels);
+				if (characters < 0) characters = 0;
+
+				characters = (int)Math.Floor(characters / 2f);
+
+				Console.Write(string.Concat(Enumerable.Repeat('=', characters))); 
+				if (characters < 25) { 
+					Console.Write(string.Concat(Enumerable.Repeat(' ', 25 - characters)));
+				}
+
+				Console.Write(string.Format(" | {0,5:00.00} dB | input {1}/{2}", levels, inputId, sourceId));
+			}
+			milliseconds = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+			//throw new NotImplementedException();
+		}
+
+	}
+}
---
+++ README.md
@@ -0,0 +1,128 @@
+https://note.com/taku_min/n/n985beda711f4
+Takumin
+2020/07/07 00:09
+
+
+ATEM mini / PRO is Fairlight audio, so the root object for audio control is
+
+`IBMDSwitcherAudioMixer` not, `IBMDSwitcherFairlightAudioMixer`
+
+Audio Input and Audio Source
+
+And while  `IBMDSwitcherAudioMixer` could simply create an AudioInput iterator and then enum it to find the Input, these models with FairlightMixer have an AudioSource iterator for each Input after iterating the AudioInput. You can get objects that you can touch variously as audio channels for the first time by creating and enum. You can see this by actually getting the object in VS2019. There are no good methods or properties attached to AudioInput.
+
+
+    internal class AtemAudioMixer
+    {
+            private IBMDSwitcherFairlightAudioMixer m_audiomixer;
+            private List<IBMDSwitcherFairlightAudioInput> m_allInputs; // all inputs
+            private List<IBMDSwitcherFairlightAudioSource> m_allSources; // all sources (iterated each input)
+            public AtemAudioMixer(IBMDSwitcher switcher)
+        {
+                this.m_audiomixer = (IBMDSwitcherFairlightAudioMixer)switcher;
+                Guid audioIteratorIID = typeof(IBMDSwitcherFairlightAudioInputIterator).GUID;
+                IntPtr audioIteratorPtr;
+                IBMDSwitcherFairlightAudioInputIterator audioIterator = null;
+                m_audiomixer.CreateIterator(ref audioIteratorIID, out audioIteratorPtr);
+                if (audioIteratorPtr != null)
+                {
+                    audioIterator = (IBMDSwitcherFairlightAudioInputIterator)Marshal.GetObjectForIUnknown(audioIteratorPtr);
+                }
+
+                m_allInputs = new List<IBMDSwitcherFairlightAudioInput>();
+                m_allSources = new List<IBMDSwitcherFairlightAudioSource>();
+                while (true)
+                {
+                    IBMDSwitcherFairlightAudioInput audioInput;
+                    audioIterator.Next(out audioInput);
+                    if (audioInput == null)
+                    {
+                        break;
+                    }
+                    m_allInputs.Add(audioInput);
+
+                    // added all sources 
+                    Guid audioSourceIteratorIID = typeof(IBMDSwitcherFairlightAudioSourceIterator).GUID;
+                    IntPtr audioSourceIteratorPtr;
+                    IBMDSwitcherFairlightAudioSourceIterator audioSourceIterator = null;
+                    audioInput.CreateIterator(ref audioSourceIteratorIID, out audioSourceIteratorPtr);
+                    if (audioSourceIteratorPtr != null)
+                    {
+                        audioSourceIterator = (IBMDSwitcherFairlightAudioSourceIterator)Marshal.GetObjectForIUnknown(audioSourceIteratorPtr);
+                    }
+                    while(true)
+                {
+                        IBMDSwitcherFairlightAudioSource audioSource;
+                        audioSourceIterator.Next(out audioSource);
+                        if (audioSource == null)
+                        {
+                            break;
+                        }
+                        m_allSources.Add(audioSource);
+                    }
+                }
+            }
+
+            public List<IBMDSwitcherFairlightAudioInput> GetAudioInputs()
+        {
+                return m_allInputs;
+            }
+
+            public List<IBMDSwitcherFairlightAudioSource> GetAudioSources()
+        {
+                return m_allSources;
+        }
+        }
+
+
+Calling code
+
+            IBMDSwitcherDiscovery discovery = new CBMDSwitcherDiscovery();
+			IBMDSwitcher switcher;
+			_BMDSwitcherConnectToFailure failureReason;
+			discovery.ConnectTo("192.168.xx.xx", out switcher, out failureReason);
+
+            // ...
+
+            AtemAudioMixer audioMixer = new AtemAudioMixer(switcher);
+			foreach(var audioInput in audioMixer.GetAudioInputs())
+            {
+				_BMDSwitcherExternalPortType portType;
+				audioInput.GetCurrentExternalPortType(out portType);
+				Console.WriteLine("AudioPortType: " + portType.ToString());
+			}
+
+			foreach(var audioSource in audioMixer.GetAudioSources())
+            {
+ 				_BMDSwitcherFairlightAudioSourceType sourceType;
+				audioSource.GetSourceType(out sourceType);
+				Console.WriteLine("AudioSourceType: " + sourceType.ToString());
+
+				double faderGain;
+				audioSource.GetFaderGain(out faderGain);
+                // 例えばフェーダーのゲインを取ってみる。
+				Console.WriteLine("FaderGain: " + faderGain.ToString());
+			}
+
+Output
+
+AudioPortType: bmdSwitcherExternalPortTypeHDMI
+AudioPortType: bmdSwitcherExternalPortTypeHDMI
+AudioPortType: bmdSwitcherExternalPortTypeHDMI
+AudioPortType: bmdSwitcherExternalPortTypeHDMI
+AudioPortType: bmdSwitcherExternalPortTypeTSJack // <-- マイクジャック1
+AudioPortType: bmdSwitcherExternalPortTypeTSJack // <-- マイクジャック2
+AudioSourceType: bmdSwitcherFairlightAudioSourceTypeStereo // HDMI 1
+FaderGain: 0
+AudioSourceType: bmdSwitcherFairlightAudioSourceTypeStereo // HDMI 2
+FaderGain: 0
+AudioSourceType: bmdSwitcherFairlightAudioSourceTypeStereo // HDMI 3
+FaderGain: 0
+AudioSourceType: bmdSwitcherFairlightAudioSourceTypeStereo // HDMI 4
+FaderGain: 0
+AudioSourceType: bmdSwitcherFairlightAudioSourceTypeStereo // Mic 1
+FaderGain: 0
+AudioSourceType: bmdSwitcherFairlightAudioSourceTypeMono // Mic 2 (mono A)
+FaderGain: 0
+AudioSourceType: bmdSwitcherFairlightAudioSourceTypeMono // Mic 2 (mono B)
+FaderGain: 0
